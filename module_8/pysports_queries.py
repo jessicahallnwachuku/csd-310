ff0797d344634c68a966573a728ab24140d37022
Jessica Hall <jess@Jessicas-MacBook-Air.local>
2023-02-02 19:50:55 -0600

    module 8 update

--- module_8/pysports_queries.py
+++ module_8/pysports_queries.py
@@ -1,15 +1,10 @@
-""" 
-    Title: pysports_queries.py
-    Author: Professor Krasso
-    Date: 15 July 2020
-    Description: Test program for executing queries against the pysports database. 
-"""
 
-""" import statements """
+
+
 import mysql.connector
 from mysql.connector import errorcode
 
-""" database config object """
+
 config = {
     "user": "pysports_user",
     "password": "MySQL8IsGreat!",
@@ -19,33 +14,30 @@ config = {
 }
 
 try:
-    """ try/catch block for handling potential MySQL database errors """ 
-
-    db = mysql.connector.connect(**config) # connect to the pysports database 
+    db = mysql.connector.connect(**config) 
 
     cursor = db.cursor()
 
-    # select query from the team table 
+
     cursor.execute("SELECT team_id, team_name, mascot FROM team")
 
-    # get the results from the cursor object 
+
     teams = cursor.fetchall()
 
     print("\n  -- DISPLAYING TEAM RECORDS --")
-    
-    # iterate over the teams data set and display the results 
+
     for team in teams: 
         print("  Team ID: {}\n  Team Name: {}\n  Mascot: {}\n".format(team[0], team[1], team[2]))
 
-    # select query for the player table 
+
     cursor.execute("SELECT player_id, first_name, last_name, team_id FROM player")
 
-    # get the results from the cursor object 
+
     players = cursor.fetchall()
 
     print ("\n  -- DISPLAYING PLAYER RECORDS --")
 
-    # iterate over the players data set and display the results
+
     for player in players:
         print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team ID: {}\n".format(player[0], player[1], player[2], player[3]))
 
@@ -64,6 +56,5 @@ except mysql.connector.Error as err:
         print(err)
 
 finally:
-    """ close the connection to MySQL """
     
     db.close()
